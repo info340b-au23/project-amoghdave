1d722e0750d3b83b9da68e20b608f2ef35c1524d
jertang <jertang@uw.edu>
2023-12-11 13:27:02 -0800

    search bar and filter component

--- src/pages/FilterComponent.js
+++ src/pages/FilterComponent.js
@@ -1,69 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function FilterComponent({ filters, setFilters }) {
-  const handleSearchChange = (event) => {
-    setFilters({ ...filters, searchTerm: event.target.value });
+const FilterComponent = ({ onFilter }) => {
+  const [filterCriteria, setFilterCriteria] = useState({
+    beds: '',
+    baths: '',
+  });
+
+  const handleChange = (e) => {
+    setFilterCriteria({ ...filterCriteria, [e.target.name]: e.target.value });
   };
 
-  const handlePriceChange = (event) => {
-    setFilters({ ...filters, priceRange: parseInt(event.target.value, 10) });
-  };
-
-  const handleBedroomChange = (event) => {
-    setFilters({ ...filters, numBedrooms: event.target.value });
-  };
-
-  const handleDateChange = (event) => {
-    setFilters({ ...filters, availabilityDate: event.target.value });
-  };
-
-  const applyFilter = () => {
-    console.log('Filters:', filters);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onFilter(filterCriteria);
   };
 
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <input
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-        className="form-control me-2"
-        value={filters.searchTerm || ''}
-        onChange={handleSearchChange}
+        type="number"
+        name="beds"
+        placeholder="Bedrooms"
+        value={filterCriteria.beds}
+        onChange={handleChange}
       />
-      <button className="btn btn-outline-success" onClick={applyFilter}>
-        Search
-      </button>
       <input
-        type="range"
-        name="priceRange"
-        min="500"
-        max="5000"
-        step="100"
-        value={filters.priceRange || 500}
-        onChange={handlePriceChange}
+        type="number"
+        name="baths"
+        placeholder="Bathrooms"
+        value={filterCriteria.baths}
+        onChange={handleChange}
       />
-      <select
-        name="numBedrooms"
-        value={filters.numBedrooms || 'studio'}
-        onChange={handleBedroomChange}
-      >
-        <option value="studio">Studio</option>
-        <option value="1">1 Bedroom</option>
-        <option value="2">2 Bedrooms</option>
-        <option value="3">3+ Bedrooms</option>
-      </select>
-      <input
-        type="date"
-        name="availabilityDate"
-        value={filters.availabilityDate || ''}
-        onChange={handleDateChange}
-      />
-      <button className="index-button" onClick={applyFilter}>
-        Apply Filter
-      </button>
-    </div>
+      <button type="submit">filter</button>
+    </form>
   );
-}
+};
 
 export default FilterComponent;
