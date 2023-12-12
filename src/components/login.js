57a117d27e579732c1453c7086e1171bd4de66ac
sgang624 <sgang624@gmail.com>
2023-12-11 20:10:06 -0800

    quick fix

--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,7 @@
 // Login.js
 import React, { useState } from 'react';
 import { getDatabase, ref, child, get  } from "firebase/database";
-import firebaseApp from 'src/firebase.js';
+// import firebaseApp from 'src/firebase.js';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -14,6 +14,8 @@ const Login = () => {
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
   };
+
+  const auth = getAuth(firebaseApp);
 
   const handleSubmit = (e) => {
     e.preventDefault();
